a727a6b6f3ec72781441d117a8f4c2785db45c33
thu <mingfei.net@gmail.com>
2017-10-20 15:12:22 +0800

    Update

--- Node.js/peiyou/process/exit.js
+++ Node.js/peiyou/process/exit.js
@@ -7,8 +7,18 @@
  * Date: 2017/10/20 11:51
  */
 
+/*
 process.exit(1);
 
 process.getgid();
 
 console.log('Current id: ' + process.getgid()); // only for POSIX /ˈpɒz.ɪks/
+*/
+
+
+process.on('exit', () => {
+    setTimeout(() => {
+        console.log('This will not run.');
+    }, 100);
+    console.log('Bye.');
+});
--- Node.js/peiyou/process/memoryUsage.js
+++ Node.js/peiyou/process/memoryUsage.js
@@ -8,3 +8,12 @@
  */
 
 console.log('Memory usage: ' + JSON.stringify(process.memoryUsage(), null, '\t'));
+
+/*
+Memory usage: {
+    "rss": 17989632, // ram
+    "heapTotal": 7372800,
+    "heapUsed": 3125240,
+    "external": 8380
+}
+*/
