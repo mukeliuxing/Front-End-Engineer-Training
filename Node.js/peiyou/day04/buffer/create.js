de7d704e25c48328d159449d5e2725e92ec65bc6
thu <mingfei.net@gmail.com>
2017-10-30 15:23:38 +0800

    Update

--- Node.js/peiyou/day04/buffer/create.js
+++ Node.js/peiyou/day04/buffer/create.js
@@ -7,5 +7,16 @@
  * Date: 2017/10/30 10:32
  */
 
-let buffer = Buffer.alloc(1024);
-console.log(buffer);
+const buf1 = Buffer.alloc(1024);
+console.log(buf1);
+
+const buf2 = Buffer.from([0x68, 0x65, 0x6c, 0x6c, 0x6f, 0x20, 0x77, 0x6f, 0x72, 0x6c, 0x64]);
+console.log(buf2);
+
+const buf3 = Buffer.from('一', 'utf8');
+console.log(buf3);
+console.log(buf3.toString('utf8'));
+
+// 11100100 10111000 10000000
+// 111001001011100010000000
+
