6eba64b117e9430ee9226b598b58b083660fff0b
thu <mingfei.net@gmail.com>
2017-10-30 10:53:02 +0800

    Update

---
+++ Node.js/peiyou/day04/buffer/create.js
@@ -0,0 +1,11 @@
+/*
+ * https://github.com/thu/Front-End-Engineer-Training
+ *
+ * Copyright mingfei.net@gmail.com
+ * Released under the MIT license
+ *
+ * Date: 2017/10/30 10:32
+ */
+
+let buffer = Buffer.alloc(1024);
+console.log(buffer);
---
+++ Node.js/peiyou/day04/test.js
@@ -0,0 +1,9 @@
+/*
+ * https://github.com/thu/Front-End-Engineer-Training
+ *
+ * Copyright mingfei.net@gmail.com
+ * Released under the MIT license
+ *
+ * Date: 2017/10/30 9:14
+ */
+
