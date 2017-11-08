bc1f63f13f43641f771ed44e51b3a815c8235929
thu <mingfei.net@gmail.com>
2017-11-08 17:46:03 +0800

    Update

--- Node.js/peiyou/day07/update.js
+++ Node.js/peiyou/day07/update.js
@@ -4,6 +4,21 @@
  * Copyright mingfei.net@gmail.com
  * Released under the MIT license
  *
- * Date: 2017/11/6 16:47
+ * Date: 2017/11/6 16:46
  */
+
+const mysql = require('mysql');
+
+let connection = mysql.createConnection({
+    user: 'root',
+    password: 'system'
+});
+
+connection.connect((error) => {
+    if (error) throw error;
+    connection.query('update scott.test set username=? where id=?', ['Jerry', '1'], (error, results, fields) => {
+        if (error) throw error;
+        console.log(results.affectedRows);
+    });
+});
 
