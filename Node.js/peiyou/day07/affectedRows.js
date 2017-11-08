e536b07ea33cc188b93083bbbaae8b02ca904136
thu <mingfei.net@gmail.com>
2017-11-08 17:44:23 +0800

    Update

--- Node.js/peiyou/day07/affectedRows.js
+++ Node.js/peiyou/day07/affectedRows.js
@@ -4,6 +4,30 @@
  * Copyright mingfei.net@gmail.com
  * Released under the MIT license
  *
- * Date: 2017/11/6 17:02
+ * Date: 2017/11/6 16:46
  */
+
+const mysql = require('mysql');
+
+let connection = mysql.createConnection({
+    user: 'root',
+    password: 'system'
+});
+
+connection.connect((error) => {
+    if(error) throw error;
+    console.log('connected...');
+    let sql = 'INSERT INTO scott.test(username, password) VALUES ?';
+    let values = [
+        ['user1', 'pass1'],
+        ['user2', 'pass2'],
+        ['user3', 'pass3']
+    ];
+    connection.query(sql, [values], (error, results, fields) => {
+        if (error) throw error;
+        console.log(results.insertId);
+        console.log(results.affectedRows);
+    });
+});
+
 
--- Node.js/peiyou/day07/delete.js
+++ Node.js/peiyou/day07/delete.js
@@ -7,3 +7,18 @@
  * Date: 2017/11/6 16:46
  */
 
+const mysql = require('mysql');
+
+let connection = mysql.createConnection({
+    user: 'root',
+    password: 'system'
+});
+
+connection.connect((error) => {
+    if (error) throw error;
+    connection.query('delete from scott.test where id=?', ['4'], (error, results, fields) => {
+        if (error) throw error;
+        console.log(results.affectedRows);
+    });
+});
+
