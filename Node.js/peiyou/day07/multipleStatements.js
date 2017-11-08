3bbc76643036cf7ff02833e40b8be3b6b357d852
thu <mingfei.net@gmail.com>
2017-11-08 17:55:47 +0800

    Update

--- Node.js/peiyou/day07/multipleStatements.js
+++ Node.js/peiyou/day07/multipleStatements.js
@@ -4,6 +4,23 @@
  * Copyright mingfei.net@gmail.com
  * Released under the MIT license
  *
- * Date: 2017/11/6 17:02
+ * Date: 2017/11/6 16:46
  */
+
+const mysql = require('mysql');
+
+let connection = mysql.createConnection({
+    user: 'root',
+    password: 'system',
+    multipleStatements: true
+});
+
+connection.connect((error) => {
+    if (error) throw error;
+    connection.query('select 1; select 2', (error, results, fields) => {
+        if (error) throw error;
+        console.log(results[0]);
+        console.log(results[1]);
+    });
+});
 
