e79319a56adaad657a2d4fd7d6555a30378d62bb
thu <mingfei.net@gmail.com>
2017-11-09 17:50:02 +0800

    Update

--- Node.js/peiyou/day08/routes/user.js
+++ Node.js/peiyou/day08/routes/user.js
@@ -17,7 +17,7 @@ let pool = mysql.createPool({
 });
 module.exports = function (app) {
     app.post('/signIn', (req, res) => {
-        pool.query('SELECT * FROM db_user_book.user WHERE username=? AND password=?', [req.body.username, req.body.password], (error, results, fields) => {
+        pool.query('SELECT * FROM db_user_book.user WHERE username = ? AND password = ?', [req.body.username, req.body.password], (error, results, fields) => {
             if (error) throw error;
             if (results.length === 1) {
                 res.sendFile(path.join(__dirname, '../public/home.html'));
@@ -26,4 +26,19 @@ module.exports = function (app) {
             }
         })
     });
+
+    app.post('/signUp', (req, res) => {
+        pool.getConnection((error, connection) => {
+            connection.query('INSERT INTO db_user_book.user VALUES(NULL, ?, ?)', [req.body.username, req.body.password], (error, results, fields) => {
+                if (error) throw error;
+                console.log(results);
+                if (results.affectedRows === 1) {
+                    res.sendFile(path.join(__dirname, '../public/index.html'))
+                } else {
+                    res.sendFile(path.join(__dirname, '../public/sign-up.html'))
+                }
+            });
+            connection.release();
+        });
+    })
 };
